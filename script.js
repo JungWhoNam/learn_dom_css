f00d14cfff34e3404dc9b01b166fd0db7fca0d8c
namxx054 <namxx054@umn.edu>
2021-05-04 17:02:45 +0900

    Adds codes for testing js injection and accessing attributes

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-
+// // append vs appendChild
 // {
 //     const body = document.body;
 
-//     body.append("blah blah blah");
+//     // html tag가 아니라 string 값으로 들어감 
+//     body.append("<br />blah blah blah");
 
 //     // body.appendChild("Hello World"); // error
 //     body.appendChild(document.createElement("h1"));
 // }
 
-// innerText vs textContent
+
+// // innerText vs textContent
 // {
 //     const div = document.querySelector("div");
 //     // only the visibile information from the page...
@@ -19,22 +21,27 @@
 // }
 
 
-//https://www.youtube.com/watch?v=ns1LX6mEvyM
 // a safter way of adding new elements to existing html
+// avoiding javascript injection
+// make sure whatever input from the users is put with "innerHTML"...
+// or put it as a string
+// https://www.youtube.com/watch?v=ns1LX6mEvyM
 // {
 //     const body = document.body;
 //     const div = document.createElement("div");
 
-//     const inputFromUser = `
-//         <script>
-//             alert("Hello~~~");
-//         </script>
-//     `;
+//     // script 태그는 작동이 안 됨 (브라우져에서 막음) 
+//     // img 태그의 error callback 사용해서 injection을 할 수 있음
+//     //const inputFromUser = '<script>alert("hello");<script>';
+//     const inputFromUser = '<img src onerror="alert(\'hi\')">';
 
-//     // Option #1
-//     // div.innerHTML = inputFromUser;
+//     // Option #1 (not safe from a javascript injection)
+//     // div.innerHTML = `<strong>${inputFromUser}</strong>`;
 
-//     // Option #2 - more readable and safe way of editing the html thru JS
+//     // Option #2 (to guard)
+//     // div.innerText = `<strong>${inputFromUser}</strong>`;
+
+//     // Option #3 - (to guard)
 //     const strong = document.createElement("strong");
 //     strong.innerText = inputFromUser; // provided by the user
 //     div.appendChild(strong);
@@ -42,8 +49,19 @@
 //     body.appendChild(div);
 // }
 
-
-//https://www.youtube.com/watch?v=y17RuWkWdn8
 {
-
+    const world = document.querySelector(".world");
+    console.log(world.getAttribute("class"));
+    // access the custom data attribute by "dataset"
+    console.log(world.dataset.test);
+    // data-longer-name은 camel case로 바뀜
+    console.log(world.dataset.longerName);
+    // access the class attributes
+    console.log(world.classList);
+    // add/remove class
+    world.classList.toggle("toggle", false);
+    // access CSS property
+    world.style.display = "block";
+    // background-color이 camel case로 바뀜
+    world.style.backgroundColor = "red";
 }
